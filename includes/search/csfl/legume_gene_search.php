c30da53bc88a09d57a0a33a625b753b7e3369364
ccheng <chunhuaicheng@gmail.com>
2016-11-21 11:47:28 -0800

    Change curate genes to genbank genes for CSFL gene search

--- includes/search/csfl/legume_gene_search.php
+++ includes/search/csfl/legume_gene_search.php
@@ -34,7 +34,7 @@ function chado_search_gene_search_form ($form) {
       ->column('analysis')
       ->table('chado_search_gene_search')
       ->multiple(TRUE)
-      ->optGroupByPattern(array('Curated Genes' => 'NCBI', 'Predicted Genes' => 'genome', 'Unigene' => 'unigene', 'RefTrans' => 'RefTrans'))
+      ->optGroupByPattern(array('Genbank Genes' => 'NCBI', 'Predicted Genes' => 'genome', 'Unigene' => 'unigene', 'RefTrans' => 'RefTrans'))
       ->cache(TRUE)
       ->labelWidth(163)
       ->size(5)
