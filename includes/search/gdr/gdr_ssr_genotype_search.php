a2269d703765ca15734616c687bd144c769f8973
Chun-Huai Cheng <chunhuaicheng@gmail.com>
2017-01-10 13:55:24 -0800

    Updated tutorial hyperlinks for GDR genotype search

--- includes/search/gdr/gdr_ssr_genotype_search.php
+++ includes/search/gdr/gdr_ssr_genotype_search.php
@@ -58,7 +58,7 @@ function chado_search_ssr_genotype_search_form ($form) {
       SNP Genotype. To search for SSR genotype data only for cultivars and breeding selections 
       please visit the <a href=\"/legacy/bt_search_genotype/by_variety\">'Search Genotyping 
       Data'</a> page in the <a href=\"/legacy/breeders_toolbox\">Breeders Toolbox</a>.
-     <b> | " . l('Short video tutorial', 'https://www.youtube.com/watch?v=ARZGxKz5mRo', array('attributes' => array('target' => '_blank'))) . ' | ' . l('Text tutorial', 'tutorial/search_snp_genotype') . ' | ' .
+     <b> | " . l('Short video tutorial', 'https://www.youtube.com/watch?v=ARZGxKz5mRo', array('attributes' => array('target' => '_blank'))) . ' | ' . l('Text tutorial', 'tutorial/search_ssr_genotype') . ' | ' .
        l('Email us with problems and suggestions', 'contact') . '</b>';
   $form->addFieldset(
       Set::fieldset()
