91aa04c5cbb4e7516ae47c6644a1f34fdc642d74
ccheng <chunhuaicheng@gmail.com>
2018-05-09 09:54:26 -0700

    Updated CottonGen SNP Genotype link

--- includes/search/cottongen/cottongen_ssr_genotype_search.php
+++ includes/search/cottongen/cottongen_ssr_genotype_search.php
@@ -11,7 +11,7 @@ function chado_search_ssr_genotype_search_form ($form) {
   $form->addTabs(
       Set::tab()
       ->id('ssr_genotype_tabs')
-      ->items(array('/find/ssr_genotype' => 'SSR Genotype', '/find/snp_genotype' => 'SNP Genotype'))
+      ->items(array('/find/ssr_genotype' => 'SSR Genotype', '/find/snp/genotype' => 'SNP Genotype'))
   );
   
   $form->addSelectFilter(
